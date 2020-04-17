9d25e2ce9e26711ad6f0807278c146e47277b5f5
Joseph Miller <jmm1@iastate.edu>
2020-04-17 11:47:17 -0500

    added some files

---
+++ lab_parts.h
@@ -0,0 +1,20 @@
+/*
+ * lab_parts.h
+ *
+ *  Created on: Apr 9, 2020
+ *      Author: joemm
+ */
+
+#ifndef LAB_PARTS_H_
+#define LAB_PARTS_H_
+
+extern volatile float m;
+extern volatile int b;
+
+void part1(void);
+void part2(void);
+void part3(void);
+void part4(void);
+void part4_check (void);
+
+#endif /* LAB_PARTS_H_ */
